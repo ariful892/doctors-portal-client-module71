4deacb67a24c45578e641c495f0a2103f1559705
ariful892 <ariful00892@gmail.com>
2022-05-14 02:02:42 +0600

    info section  added in home page

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
 
@@ -6,9 +10,11 @@ const Info = () => {
     const bgAccent = 'bg-accent';
 
     return (
-        <div>
-
-        </div>
+        <section className='grid grid-cols-1 lg:grid-cols-3 gap-5 my-20'>
+            <InfoCard cardTitle="Opening Hours" img={clock} bgClass={bgPrimary}></InfoCard>
+            <InfoCard cardTitle="Our Locations" img={marker} bgClass={bgAccent}></InfoCard>
+            <InfoCard cardTitle="Contact Us" img={phone} bgClass={bgPrimary}></InfoCard>
+        </section>
     );
 };
 
